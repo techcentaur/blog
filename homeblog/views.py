5e6f9a8baeac2681f07e641d256a13f0e541a615
techcentaur <ankit03june@gmail.com>
2017-03-06 09:32:08 -0800

    Set up for Heroku

--- homeblog/views.py
+++ homeblog/views.py
@@ -2,19 +2,19 @@ from .models import Blog, Category
 from django.shortcuts import render, get_object_or_404
 
 def index(request):
-    return render('index.html', {
+    return render(request, 'index.html', {
         'categories': Category.objects.all(),
         'posts': Blog.objects.all()[:5]
     })
 
 def view_post(request, slug):   
-    return render('view_post.html', {
+    return render(request, 'view_post.html', {
         'post': get_object_or_404(Blog, slug=slug)
     })
 
 def view_category(request, slug):
     category = get_object_or_404(Category, slug=slug)
-    return render('view_category.html', {
+    return render(request, 'view_category.html', {
         'category': category,
         'posts': Blog.objects.filter(category=category)[:5]
     })
