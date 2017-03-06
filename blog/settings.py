a29e0b8fa613e043da24cf081f06e7316c617f56
techcentaur <ankit03june@gmail.com>
2017-03-06 09:36:11 -0800

    Added allowed hosts

--- blog/settings.py
+++ blog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'n7b@=f*6r6rh9@ah&$*zl3r^5#amse=2unyyuthqa_#x1yuof7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['techcentaur-blog.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
