43fb5f0399068a30059c97a3049d9e0359484dca
techcentaur <ankit03june@gmail.com>
2017-03-06 08:46:26 -0800

    without error first commit

--- homeblog/urls.py
+++ homeblog/urls.py
@@ -2,13 +2,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-url(r'^$', 'views.index'),
+url(r'^$', views.index),
 url(
     r'^blog/view/(?P<slug>[^\.]+).html', 
-    'views.view_post', 
+    views.view_post, 
     name='view_blog_post'),
 url(
     r'^blog/category/(?P<slug>[^\.]+).html', 
-    'views.view_category', 
+    views.view_category, 
     name='view_blog_category'),
 ]
--- homeblog/views.py
+++ homeblog/views.py
@@ -1,20 +1,20 @@
 from .models import Blog, Category
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
-    return render_to_response('index.html', {
+    return render('index.html', {
         'categories': Category.objects.all(),
         'posts': Blog.objects.all()[:5]
     })
 
 def view_post(request, slug):   
-    return render_to_response('view_post.html', {
+    return render('view_post.html', {
         'post': get_object_or_404(Blog, slug=slug)
     })
 
 def view_category(request, slug):
     category = get_object_or_404(Category, slug=slug)
-    return render_to_response('view_category.html', {
+    return render('view_category.html', {
         'category': category,
         'posts': Blog.objects.filter(category=category)[:5]
     })
